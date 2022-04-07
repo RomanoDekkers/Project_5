167f9b54ae29725dc4323959f81858b9f45fcec0
rodi janssen <rodi.janssen@rocdeleijgraaf.nl>
2022-04-07 09:37:38 +0200

    connect.php toegevoegt

---
+++ connect.php
@@ -0,0 +1,20 @@
+<?php
+$dbServername = "127.0.0.1:3307";
+$dbUsername = "root";
+$dbPasword = "";
+$dbname = "fiets_verhuur_de_elstar";
+$charset = 'utf8mb4';
+$conn = mysqli_connect("$dbServername", "$dbUsername", "$dbPasword", "$dbname");
+
+$dsn = "mysql:host=$dbServername;dbname=$dbname;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try {
+    $pdo = new PDO($dsn, $dbUsername, $dbPasword, $options);
+} 
+catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
--- index.php
+++ index.php
@@ -5,26 +5,8 @@
     <body>
           <h1>Start</h1> 
           <?php
-            $dbServername = "127.0.0.1:3307";
-            $dbUsername = "root";
-            $dbPasword = "";
-            $dbname = "fiets_verhuur_de_elstar";
-            $charset = 'utf8mb4';
-            $conn = mysqli_connect("$dbServername", "$dbUsername", "$dbPasword", "$dbname");
-
-            $dsn = "mysql:host=$dbServername;dbname=$dbname;charset=$charset";
-            $options = [
-                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                PDO::ATTR_EMULATE_PREPARES   => false,
-            ];
-            try {
-                $pdo = new PDO($dsn, $dbUsername, $dbPasword, $options);
-            } 
-            catch (\PDOException $e) {
-                throw new \PDOException($e->getMessage(), (int)$e->getCode());
-            }
-
+            include("connect.php")
+            
             $stmt = $pdo->query("INSERT INTO klanten (naam, adres, woonplaats, postcode, E_mail, telefoon_nummer)
             VALUES ('test_naam', 'test_adres', 'cuijk', '5387hi', 'test@test.com', '0612345678')");
         ?>
