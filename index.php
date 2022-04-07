ed71e1602e5c1823c156f6bb1f1ef28c40c5ac5f
rodi janssen <rodi.janssen@rocdeleijgraaf.nl>
2022-04-07 09:44:16 +0200

    ;

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <body>
           <h1>Start</h1> 
           <?php
-            include("connect.php")
+            include("connect.php");
             
             $stmt = $pdo->query("INSERT INTO klanten (naam, adres, woonplaats, postcode, E_mail, telefoon_nummer)
             VALUES ('test_naam', 'test_adres', 'cuijk', '5387hi', 'test@test.com', '0612345678')");
