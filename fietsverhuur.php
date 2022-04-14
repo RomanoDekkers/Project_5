dbb2ffaaecdaf814d6bdded4f0c0d835cc6f2c3b
rodi janssen <rodi.janssen@rocdeleijgraaf.nl>
2022-04-14 10:16:56 +0200

    lijst met alle fietsen

--- fietsverhuur.php
+++ fietsverhuur.php
@@ -1,10 +1,43 @@
 <html>
     <head>
         <title>fietsverhuur de elstar</title>
+        <link rel="stylesheet" href="index.css">
     </head>
     <body>
-          <h1>Start</h1> 
-          <?php
-          ?>
+        <h1>Start</h1> 
+        <?php
+        $tabelData = '<tr>
+            <td>ID</td>
+            <td>merk_ID</td>
+            <td>heren_dames_uni</td>
+            <td colspan="2">opties</td>
+        </tr>';
+            include("connect.php");
+            $stmt = $pdo->query("SELECT * FROM fiets");
+
+            foreach ($stmt as $rij){
+                $tabelData .= '<tr>';
+                    $tabelData .= '<td>';
+                        $tabelData .= $rij['ID'];
+                    $tabelData .= '</td>';
+                    $tabelData .= '<td>';
+                        $tabelData .= $rij['merk_ID'];
+                    $tabelData .= '</td>';
+                    $tabelData .= '<td>';
+                        $tabelData .= $rij['heren_dames_uni'];
+                    $tabelData .= '</td>';
+                    $tabelData .= '<td>';
+                        $tabelData .= $rij['maat'];
+                    $tabelData .= '</td>';
+                    $tabelData .= '<td>';
+                        $tabelData .= $rij['prijs'];
+                    $tabelData .= '</td>';
+                    $tabelData .= '<td>';
+                        $tabelData .= $rij['fiets_serienummer'];
+                    $tabelData .= '</td>';
+                $tabelData .= '</tr>';
+            }
+            echo $tabelData;
+        ?>
     </body>
 </html>
