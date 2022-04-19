345cf2c0b43f49572239ee6787f2b356582bbd38
Romano Dekkers <0085420@rocdeleijgraaf.nl>
2022-04-19 10:48:14 +0200

    table

--- fietsverhuur.php
+++ fietsverhuur.php
@@ -5,8 +5,28 @@
     </head>
     <body>
         <h1>Start</h1> 
+        <script>function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
+</script>
         <?php
-        $tabelData = '<tr>
+        $tabelData = '<table border="1" width="500px"><tr>
             <td>ID</td>
             <td>merk_ID</td>
             <td>heren_dames_uni</td>
@@ -39,7 +59,21 @@
                         $tabelData .= $rij['fiets_serienummer'];
                     $tabelData .= '</td>';
                 $tabelData .= '</tr>';
+                $tabelData .= '<div class="dropdown">
+                <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+                <div id="myDropdown" class="dropdown-content">
+                  <input type="text" placeholder="Search.." id="myInput" onkeyup="filterFunction()">
+                  <a href="#about">About</a>
+                  <a href="#base">Base</a>
+                  <a href="#blog">Blog</a>
+                  <a href="#contact">Contact</a>
+                  <a href="#custom">Custom</a>
+                  <a href="#support">Support</a>
+                  <a href="#tools">Tools</a>
+                </div>
+              </div>';
             }
+            $tabelData .= '</table>';
             echo $tabelData;
         ?>
     </body>
