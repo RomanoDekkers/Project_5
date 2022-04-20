a30435ab77ac0cf5a16fc9fa491735049f68a034
Romano Dekkers <0085420@rocdeleijgraaf.nl>
2022-04-20 09:34:12 +0200

    update feature

--- fietsen.php
+++ fietsen.php
@@ -66,6 +66,19 @@
             </table>
         </form>
         <?php
+
+if(isset($_POST)){
+	if(isset($_POST['update'])){
+	$naam = $_POST['naam'];
+	$prijs = $_POST['prijs'];
+	$id = $_POST['id'];
+	
+	$sql = "UPDATE `fiets` SET `maat` = ?, `prijs` = ? WHERE `fiets`.`ID` = ?;";
+	$pdo->prepare($sql)->execute([$naam, $prijs, $id]);
+	}
+}
+
+
 if(isset($_GET['aktie'])){
         if($_GET['aktie'] == "verwijder"){
             $query = 'DELETE FROM `fiets` WHERE `ID` = ' .$_GET['id'];
@@ -74,7 +87,49 @@ if(isset($_GET['aktie'])){
             $message = 'de aktie is gelukt!';
         }
         }
+    
+
+    if($_GET['aktie'] == "wijzigen"){
+        $id = $_GET['id'];
+            $stmt = $pdo->prepare("select maat as maat, prijs as prijs from fiets where ID = ?");
+            $stmt->execute([$id]);
+            $data = $stmt->fetch();
+
+            echo "
+            <form action='fietsen.php' method='POST' name='update'>
+            <table border=1>
+            <tr>
+                <td>
+                    Categorie
+                </td>
+                <td>
+                    maat
+                </td>
+                <td colspan=2>
+                    Prijs
+                </td>
+
+            </tr>
+            <tr>
+                <td>Categorie</td>
+                <td>
+                    <input type='text' name='naam' value='".$data['maat']."'>
+                </td>
+                <td>
+                    <input type='text' name='prijs' value='".$data['prijs']."'>
+                </td>
+                <td>
+                    <input type='hidden' name='id' value='".$id."'>
+                    <input type='submit' name='update' value='Wijzig product'>
+                </td>
+            </tr>
+            </form>
+            
+            ";
+            exit;
+        }
     }
+
         $tabelData = '<table border="1" width="500px"><tr>
             <td>ID</td>
             <td>ID Merk</td>
@@ -106,6 +161,9 @@ if(isset($_GET['aktie'])){
                     $tabelData .= '<td>';
                         $tabelData .= $rij['fiets_serienummer'];
                     $tabelData .= '</td>';
+                $tabelData .= '<td>';
+                $tabelData .= '<a href="fietsen.php?aktie=wijzigen&id='.$rij['ID'].'">U</a>';
+                $tabelData .= '</td>';
                 $tabelData .= '<td>';
                 $tabelData .= '<a href="fietsen.php?aktie=verwijder&id='.$rij['ID'].'">D</a>';
                 $tabelData .= '</td>';
